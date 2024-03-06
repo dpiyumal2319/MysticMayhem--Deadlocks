f9c5b3ad377e4a973fc4df655f85c393a9bbc5a2
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-03-07 00:44:41 +0530

    Make warrior classes

---
+++ MysticMayhemJava/src/Warrior/Warrior.java
@@ -0,0 +1,10 @@
+package Warrior;
+
+abstract class Warrior {
+    protected String name;
+    protected int health, attack, defense, speed;
+    
+    protected int extraHealth, extraAttack, extraDefense, extraSpeed;
+    protected float value;
+
+}
