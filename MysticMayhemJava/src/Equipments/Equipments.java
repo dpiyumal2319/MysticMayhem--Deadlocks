a22515a851a569d1e05f28a1f3620b9619ac8c0a
dpiyumal2319 <w.piyumal2319@gmail.com>
2024-03-07 00:23:40 +0530

    ShortenDeclaration0

--- MysticMayhemJava/src/Equipments/Equipments.java
+++ MysticMayhemJava/src/Equipments/Equipments.java
@@ -2,9 +2,5 @@ package Equipments;
 
 public class Equipments {
     protected String name;
-    protected int extraAttack;
-    protected int extraDefense;
-    protected int extraSpeed;
-    protected int extraHealth;
-    protected int price;
+    protected int extraAttack, extraDefense, extraSpeed, extraHealth, price;
 }
