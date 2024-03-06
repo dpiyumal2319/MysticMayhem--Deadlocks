6db0cc8f8537c689dd06d71c7ccbdb581f5ac968
dpiyumal2319 <w.piyumal2319@gmail.com>
2024-03-07 02:45:52 +0530

    Make Warrior method public

--- MysticMayhemJava/src/Warrior/Warrior.java
+++ MysticMayhemJava/src/Warrior/Warrior.java
@@ -1,10 +1,9 @@
 package Warrior;
 
-abstract class Warrior {
+abstract public class Warrior {
     protected String name;
     protected int health, attack, defense, speed, price;
     
     protected int extraHealth, extraAttack, extraDefense, extraSpeed;
     protected float value;
-
 }
