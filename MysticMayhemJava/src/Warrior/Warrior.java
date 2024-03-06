fb8a1ea9cbff315ead1e087683ec2ad5c6d421e3
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-03-07 00:50:20 +0530

    Update Warrior.java

--- MysticMayhemJava/src/Warrior/Warrior.java
+++ MysticMayhemJava/src/Warrior/Warrior.java
@@ -2,7 +2,7 @@ package Warrior;
 
 abstract class Warrior {
     protected String name;
-    protected int health, attack, defense, speed;
+    protected int health, attack, defense, speed, price;
     
     protected int extraHealth, extraAttack, extraDefense, extraSpeed;
     protected float value;
