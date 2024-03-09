2f9429d2617e91739bda9bbf53be816f47587427
dpiyumal2319 <w.piyumal2319@gmail.com>
2024-03-09 08:52:52 +0530

    Added Required Getters 0

--- MysticMayhemJava/src/UserManager/User.java
+++ MysticMayhemJava/src/UserManager/User.java
@@ -1,5 +1,8 @@
 package UserManager;
+import Equipments.Equipments;
 import Warrior.Warrior;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     public static int UserCount = 100000;
@@ -7,7 +10,7 @@ public class User {
     public int xp;
     public final String userName;
     private float money;
-    public Warrior Archer, Healer, Knight, Mage, MythicalCreature = null;
+    public Warrior Archer , Healer, Knight, Mage, MythicalCreature = null;
     public User(String userName) {
         this.userID = UserCount++;
         this.xp = 0;
@@ -22,5 +25,15 @@ public class User {
     }
     public void updateXP(int xp) {
         this.xp += xp;
+    }
+
+    public Map<Warrior, Equipments[]> getInventory() {
+        Map<Warrior, Equipments[]> inventory = new HashMap<Warrior, Equipments[]>();
+        inventory.put(Archer, Archer.getEquipments());
+        inventory.put(Healer, Healer.getEquipments());
+        inventory.put(Knight, Knight.getEquipments());
+        inventory.put(Mage, Mage.getEquipments());
+        inventory.put(MythicalCreature, MythicalCreature.getEquipments());
+        return inventory;
     }
 }
--- MysticMayhemJava/src/Warrior/Warrior.java
+++ MysticMayhemJava/src/Warrior/Warrior.java
@@ -1,9 +1,21 @@
 package Warrior;
 
+import Equipments.Equipments;
+
 abstract public class Warrior {
     protected String name;
     protected int health, attack, defense, speed, price;
     
     protected int extraHealth, extraAttack, extraDefense, extraSpeed;
     protected float value;
+    protected Equipments armor, artefact = null;
+
+    public Equipments[] getEquipments() {
+        Equipments[] equipments = {armor, artefact};
+        return equipments;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
