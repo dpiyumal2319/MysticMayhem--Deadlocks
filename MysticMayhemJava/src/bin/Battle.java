94a52180c6868d6bdc3e2239155178b6c3b5f426
dpiyumal2319 <w.piyumal2319@gmail.com>
2024-03-13 12:32:14 +0530

    UpdatedBattleMethod

--- MysticMayhemJava/src/bin/Battle.java
+++ MysticMayhemJava/src/bin/Battle.java
@@ -1,9 +1,9 @@
 package bin;
 
+import bin.Warriors.*;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,35 +19,39 @@ public abstract class Battle {
     private static int m = 0;
     private static int n = 0;
 
-    public static void start(User user, Map<String, User> users) {
+    public static void start(User currentUser, Map<String, User> users) {
         System.out.println("Battle started!");
         // Choosing oponent
-        User user2 = selectOponent(users, user.getxp());
-        String battleGround = user2.homeGround;
+        User opponent = selectOponent(users, currentUser);
+        String battleGround = opponent.homeGround;
 
         // Settiing up arrays for the battle
         // user array
-        Warrior[] user1Attack = { user.squad.getArcher(battleGround), user.squad.getKnight(battleGround),
-                user.squad.getMage(battleGround), user.squad.getHealer(battleGround),user.squad.getMythicalCreature(battleGround) };
-        Warrior[] user1Defence = { user.squad.getArcher(battleGround), user.squad.getKnight(battleGround),
-                user.squad.getMage(battleGround), user.squad.getHealer(),user.squad.getMythicalCreature(battleGround) };
+        Warrior[] user1Attack = { currentUser.squad.getArcher(battleGround), currentUser.squad.getKnight(battleGround),
+                currentUser.squad.getMage(battleGround), currentUser.squad.getHealer(battleGround),
+                currentUser.squad.getMythicalCreature(battleGround) };
+        Warrior[] user1Defence = { currentUser.squad.getArcher(battleGround), currentUser.squad.getKnight(battleGround),
+                currentUser.squad.getMage(battleGround), currentUser.squad.getHealer(),
+                currentUser.squad.getMythicalCreature(battleGround) };
 
         // oponent array
-        Warrior[] user2Attack = { user2.squad.getArcher(battleGround), user2.squad.getKnight(battleGround),
-                user2.squad.getMage(battleGround), user2.squad.getHealer(battleGround),user2.squad.getMythicalCreature(battleGround) };
-        Warrior[] user2Defence = { user2.squad.getArcher(battleGround), user2.squad.getKnight(battleGround),
-                user2.squad.getMage(battleGround), user2.squad.getHealer(), user2.squad.getMythicalCreature(battleGround) };
+        Warrior[] user2Attack = { opponent.squad.getArcher(battleGround), opponent.squad.getKnight(battleGround),
+                opponent.squad.getMage(battleGround), opponent.squad.getHealer(battleGround),
+                opponent.squad.getMythicalCreature(battleGround) };
+        Warrior[] user2Defence = { opponent.squad.getArcher(battleGround), opponent.squad.getKnight(battleGround),
+                opponent.squad.getMage(battleGround), opponent.squad.getHealer(),
+                opponent.squad.getMythicalCreature(battleGround) };
 
         // Soring arrays
         Arrays.sort(user2Attack, Comparator.comparing(Warrior::getBattleSpeed, Comparator.reverseOrder())
                 .thenComparing(Warrior::getAttackPriority));
-        Arrays.sort(user2Defence, Comparator.comparing(Warrior::getBattleDefense, Comparator.reverseOrder())
-                .thenComparing(Warrior::getDefensePriority));
+        Arrays.sort(user2Defence, Comparator.comparing(Warrior::getBattleDefense)
+                .thenComparing(Warrior::getDefensePriority, Comparator.reverseOrder()));
 
         Arrays.sort(user1Attack, Comparator.comparing(Warrior::getBattleSpeed, Comparator.reverseOrder())
                 .thenComparing(Warrior::getAttackPriority));
-        Arrays.sort(user1Defence, Comparator.comparing(Warrior::getBattleDefense, Comparator.reverseOrder())
-                .thenComparing(Warrior::getDefensePriority));
+        Arrays.sort(user1Defence, Comparator.comparing(Warrior::getBattleDefense)
+                .thenComparing(Warrior::getDefensePriority, Comparator.reverseOrder()));
 
         // Battle 10 turns
         Warrior attacker;
@@ -55,59 +59,79 @@ public abstract class Battle {
         for (int i = 0; i < 10; i++) {
             if (!isBattleStillGoing(user1Defence, user2Defence))
                 break;
-
             // User1 attack
             attacker = getWarriorUsr1Atk(user1Attack);
-            defender = getWarriorUsr2Def(user2Defence);
-            if (attacker.type == "Healer")
+            if (attacker instanceof Healer)
                 defender = getLowestHealthWarrior(user1Defence);
+            else
+                defender = getWarriorUsr2Def(user2Defence);
             attack(attacker, defender);
-            System.out.println("Your " + attacker.type + "[" + attacker.battleHealth + "] " + attacker.name
-                    + " attacked " + user2.userName + "'s " + defender.type + "[" + defender.battleHealth + "] "
-                    + defender.name);
-            if (isAlive(defender))
+            if (attacker instanceof Healer)
+                System.out.println("Your " + attacker.type + "[" + attacker.battleHealth + "] " + attacker.name
+                        + " healed your " + defender.type + "[" + defender.battleHealth + "] "
+                        + defender.name);
+            else
+                System.out.println("Your " + attacker.type + "[" + attacker.battleHealth + "] " + attacker.name
+                        + " attacked " + opponent.userName + "'s " + defender.type + "[" + defender.battleHealth + "] "
+                        + defender.name);
+            if (attacker.bonusTurns > 0) {
+                if (attacker instanceof Healer)
+                    defender = getLowestHealthWarrior(user1Defence);
+                else
+                    defender = getWarriorUsr2Def(user2Defence);
                 bonusAttack(attacker, defender);
+            }
             if (!isBattleStillGoing(user1Defence, user2Defence))
                 break;
 
             // User2 attack
             attacker = getWarriorUsr2Atk(user2Attack);
-            defender = getWarriorUsr1Def(user1Defence);
-            if (attacker.type == "Healer")
+            if (attacker instanceof Healer)
                 defender = getLowestHealthWarrior(user2Defence);
+            else
+                defender = getWarriorUsr1Def(user1Defence);
             attack(attacker, defender);
-            System.out
-                    .println(user2.userName + "'s " + attacker.type + "[" + attacker.battleHealth + "] " + attacker.name
-                            + " attacked your " + defender.type + "[" + defender.battleHealth + "] " + defender.name);
-            if (isAlive(defender))
-                bonusAttack(attacker, defender);
+            if (attacker instanceof Healer)
+                System.out.println(opponent.userName + "'s " + attacker.type + "[" + attacker.battleHealth + "] "
+                        + attacker.name + " healed your " + defender.type + "[" + defender.battleHealth + "] "
+                        + defender.name);
+            else
+                System.out.println(opponent.userName + "'s " + attacker.type + "[" + attacker.battleHealth + "] "
+                        + attacker.name + " attacked " + "your " + defender.type + "[" + defender.battleHealth + "] "
+                        + defender.name);
+            if (attacker.bonusTurns > 0) {
+                if (attacker.type == "Healer")
+                    defender = getLowestHealthWarrior(user2Defence);
+                else
+                    defender = getWarriorUsr2Def(user1Defence);
+            }
         }
         if (!isBattleStillGoing(user1Defence, user2Defence)) {
             if (isUser1Won(user1Defence, user2Defence)) {
-                System.out.println( user.userID+" won!");
-                user.increaseXp(1);
-                int exchangeMoney = user2.getMoney() / 10;
-                user.increaseMoney(exchangeMoney);
-                user2.decreaseMoney(exchangeMoney);
+                System.out.println(currentUser.userID + " won!");
+                currentUser.increaseXp(1);
+                int exchangeMoney = opponent.getMoney() / 10;
+                currentUser.increaseMoney(exchangeMoney);
+                opponent.decreaseMoney(exchangeMoney);
             } else {
-                System.out.println(user2.userName+" won!");
-                user2.increaseXp(1);
-                int exchangeMoney = user.getMoney() / 10;
-                user2.increaseMoney(exchangeMoney);
-                user.decreaseMoney(exchangeMoney);
+                System.out.println(opponent.userName + " won!");
+                opponent.increaseXp(1);
+                int exchangeMoney = currentUser.getMoney() / 10;
+                opponent.increaseMoney(exchangeMoney);
+                currentUser.decreaseMoney(exchangeMoney);
             }
         } else {
             System.out.println("Draw!");
         }
-        user.squad.resetBattle();
-        user2.squad.resetBattle();
+        currentUser.squad.resetBattle();
+        opponent.squad.resetBattle();
         i = 0;
         j = 0;
         m = 0;
         n = 0;
     }
 
-    public static User selectOponent(Map<String, User> users, int CurrentXP) {
+    public static User selectOponent(Map<String, User> users, User currentUser) {
 
         while (true) {
             // random umteger below 10
@@ -120,14 +144,18 @@ public abstract class Battle {
 
             // Get the user from the random entry
             User randomUser = randomEntry.getValue();
-            System.out.println("Do you want to battle with "+ randomUser.userName+" : "+randomUser.xp);
+            if (randomUser == currentUser) continue;
+            System.out.println("Do you want to battle with " + randomUser.userName + " : " + randomUser.xp);
             Scanner scnner = new Scanner(System.in);
             while (true) {
                 String choice = scnner.nextLine();
                 System.out.println("Enter your choice[Y]/[N]");
-                if (choice.equalsIgnoreCase("Y")) return randomUser;
-                else if(choice.equalsIgnoreCase("N")) break;
-                else System.out.println("Wrong Choice");
+                if (choice.equalsIgnoreCase("Y")) {
+                    return randomUser;
+                } else if (choice.equalsIgnoreCase("N"))
+                    break;
+                else
+                    System.out.println("Wrong Choice");
             }
         }
 
@@ -197,14 +225,14 @@ public abstract class Battle {
     public static Boolean isBattleStillGoing(Warrior[] user1, Warrior[] user2) {
         Boolean user1Alive = false;
         Boolean user2Alive = false;
-        for (int i = 0; i < 5; i++) {
-            if (isAlive(user1[i])) {
+        for (int n = 0; n < 5; n++) {
+            if (isAlive(user1[n])) {
                 user1Alive = true;
                 break;
             }
         }
-        for (int i = 0; i < 5; i++) {
-            if (isAlive(user2[i])) {
+        for (int n = 0; n < 5; n++) {
+            if (isAlive(user2[n])) {
                 user2Alive = true;
                 break;
             }
@@ -213,8 +241,8 @@ public abstract class Battle {
     }
 
     public static Boolean isUser1Won(Warrior[] user1, Warrior[] user2) {
-        for (int i = 0; i < 5; i++) {
-            if (isAlive(user2[i])) {
+        for (int x = 0; x < 5; x++) {
+            if (isAlive(user2[x])) {
                 return false;
             }
         }
@@ -222,50 +250,62 @@ public abstract class Battle {
     }
 
     public static Warrior getWarriorUsr1Atk(Warrior[] warrior) {
+        Warrior warr = null;
         while (true) {
             if (isAlive(warrior[i])) {
-                return warrior[i];
+                warr = warrior[i];
             }
             i++;
             if (i == 5) {
                 i = 0;
             }
+            if (warr != null)
+                return warr;
         }
     }
 
     public static Warrior getWarriorUsr2Atk(Warrior[] warrior) {
+        Warrior warr = null;
         while (true) {
             if (isAlive(warrior[j])) {
-                return warrior[j];
+                warr = warrior[j];
             }
             j++;
             if (j == 5) {
                 j = 0;
             }
+            if (warr != null)
+                return warr;
         }
     }
 
     public static Warrior getWarriorUsr1Def(Warrior[] warrior) {
+        Warrior warr = null;
         while (true) {
             if (isAlive(warrior[m])) {
-                return warrior[m];
+                warr = warrior[m];
             }
             m++;
             if (m == 5) {
                 m = 0;
             }
+            if (warr != null)
+                return warr;
         }
     }
 
     public static Warrior getWarriorUsr2Def(Warrior[] warrior) {
+        Warrior warr = null;
         while (true) {
             if (isAlive(warrior[n])) {
-                return warrior[n];
+                warr = warrior[n];
             }
             n++;
             if (n == 5) {
                 n = 0;
             }
+            if (warr != null)
+                return warr;
         }
     }
 
@@ -278,16 +318,17 @@ public abstract class Battle {
             }
         }
         for (int l = 0; l < 5; l++) {
-            if (warrior[i].battleHealth < lowestHealthWarrior.battleHealth && isAlive(warrior[i])) {
-                lowestHealthWarrior = warrior[i];
+            if (warrior[l].battleHealth < lowestHealthWarrior.battleHealth && isAlive(warrior[l])) {
+                lowestHealthWarrior = warrior[l];
             }
         }
         return lowestHealthWarrior;
     }
 
     public static void attack(Warrior attacker, Warrior defWarrior) {
-        if (attacker.type == "Healer") {
-            defWarrior.battleHealth += attacker.battleAttack;
+        if (attacker instanceof Healer) {
+            defWarrior.battleHealth += attacker.battleAttack * 0.1;
+            attacker.battleHealth += attacker.healPerAttack * attacker.battleHealth;
             return;
         }
         defWarrior.battleHealth -= (0.5 * attacker.battleAttack - 0.1 * defWarrior.battleDefense);
@@ -304,100 +345,3 @@ public abstract class Battle {
         }
     }
 }
-
-/*
- * public class Main {
- * public static void main(String[] args) {
- * // Your map
- * Map<String, Integer> map = new HashMap<>();
- * map.put("A", 5);
- * map.put("B", 10);
- * map.put("C", 15);
- * map.put("D", 20);
- * 
- * // Given number
- * int givenNumber = 12;
- * 
- * // Calculate distances and weights
- * Map<String, Double> weights = new HashMap<>();
- * double totalDistance = 0;
- * for (Map.Entry<String, Integer> entry : map.entrySet()) {
- * int distance = Math.abs(entry.getValue() - givenNumber);
- * double weight = 1.0 / distance;
- * weights.put(entry.getKey(), weight);
- * totalDistance += weight;
- * }
- * 
- * // Normalize weights
- * for (Map.Entry<String, Double> entry : weights.entrySet()) {
- * entry.setValue(entry.getValue() / totalDistance);
- * }
- * 
- * // Select random value based on weights
- * Random random = new Random();
- * double rand = random.nextDouble();
- * double sum = 0;
- * String selectedKey = null;
- * for (Map.Entry<String, Double> entry : weights.entrySet()) {
- * sum += entry.getValue();
- * if (rand <= sum) {
- * selectedKey = entry.getKey();
- * break;
- * }
- * }
- * 
- * // Output
- * System.out.println("Selected key: " + selectedKey);
- * }
- * }
- */
-
-/*
- * import java.util.Arrays;
- * import java.util.Comparator;
- * 
- * class Some {
- * int attribute1;
- * int attribute2;
- * 
- * public Some(int attribute1, int attribute2) {
- * this.attribute1 = attribute1;
- * this.attribute2 = attribute2;
- * }
- * 
- * public int getAttribute1() {
- * return attribute1;
- * }
- * 
- * public int getAttribute2() {
- * return attribute2;
- * }
- * 
- * @Override
- * public String toString() {
- * return "Some{" +
- * "attribute1=" + attribute1 +
- * ", attribute2=" + attribute2 +
- * '}';
- * }
- * }
- * 
- * public class Main {
- * public static void main(String[] args) {
- * Some[] array = {
- * new Some(1, 5),
- * new Some(2, 6),
- * new Some(2, 4),
- * new Some(3, 2),
- * new Some(1, 3),
- * new Some(2, 2)
- * };
- * 
- * Arrays.sort(array, Comparator
- * .comparing(Some::getAttribute1, Comparator.reverseOrder())
- * .thenComparing(Some::getAttribute2));
- * 
- * System.out.println(Arrays.toString(array));
- * }
- * }
- */
